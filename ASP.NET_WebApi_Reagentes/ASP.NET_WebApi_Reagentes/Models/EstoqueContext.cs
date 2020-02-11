492e88608aaea181a99b0b4f9a7fd8fafebabc2e
Vitor Perez <vitor.g.perez@hotmail.com>
2020-02-11 15:57:21 -0300

    Adicionado Contexto das classes para comunicação com banco de dados

---
+++ ASP.NET_WebApi_Reagentes/ASP.NET_WebApi_Reagentes/Models/EstoqueContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_WebApi_Reagentes.Models
+{
+    public class EstoqueContext : DbContext
+    {
+        public EstoqueContext() : base("EstoqueDB")
+        {                
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Reagentes> Reagentes { get; set; }
+        public DbSet<Emprestimo> Emprestimos { get; set; }
+    }
+}
